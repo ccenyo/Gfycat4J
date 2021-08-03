d7564b14e012d631c57b3c7f9d3b15779cf966f8
NyoCe <medewou@gmail.com>
2021-08-04 00:55:52 +0200

    Update GfycatAuthTest.java

--- src/test/java/GfycatAuthTest.java
+++ src/test/java/GfycatAuthTest.java
@@ -5,8 +5,6 @@ public class GfycatAuthTest {
 
     @Test
     public void connect_and_success() {
-        String clientId = "2_Dbuv6W";
-        String clientSecret = "VwRvvJ-FvA3BqpkJLAE5FMWLzb5yGgeIhDVRSwd6ypv7YBPEj_wNcV0SP3JlDD0-";
 
         GfycatClient client =  GfycatBuilder.connect(clientId, clientSecret);
 
